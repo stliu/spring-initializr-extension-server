ec9a65bb8dd6e1f349ac343b40ec54e4b047238b
Strong Liu <shaozliu@microsoft.com>
2023-01-22 10:17:00 +0800

    Initial commit

---
+++ src/main/java/club/stliu/spring/initializr/extension/SpringInitializrExtensionServerApplication.java
@@ -0,0 +1,13 @@
+package club.stliu.spring.initializr.extension;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrExtensionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializrExtensionServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/stliu/spring/initializr/extension/SpringInitializrExtensionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package club.stliu.spring.initializr.extension;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializrExtensionServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
